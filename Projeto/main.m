0092de950782855b76461fa3aa7f6c368250af99
Bruno Grifo <leitaogrifo@gmail.com>
2018-04-12 20:58:49 +0100

    ficha tp4. Aproximação Linear grau2

--- Projeto/main.m
+++ Projeto/main.m
@@ -30,3 +30,49 @@ fprintf('Media: %d\n',media);
 desvio=std(values);
 fprintf('Desvio Padrão: %d\n',desvio);
 
+%Verificar outliers
+N=size(values,1);
+MeanMat1=repmat(media,N,1); %repete media (mu) em N linhas
+SigmaMat1=repmat(desvio,N,1); %repete desvio padrão em N linhas
+outliers = find(abs(values - MeanMat1)>3*SigmaMat1); % identifica os outliers
+N_outliers=length(outliers);  %numero de outliers
+outliers=[];
+for i=1:N
+    if (abs(values(i) - media) > 3*desvio)
+        %é outlier
+        outliers=[outliers, i]; %#ok<AGROW>
+    end
+end
+
+new_values=values;%substituição do outlier
+
+if N_outliers
+     for k=outliers
+         if new_values(k)> media
+             new_values(k) = media + 2.5*desvio;
+         else
+             new_values = media - 2.5*desvio;
+         end
+     end
+end
+figure(2)
+subplot(1,2,1)
+plot(values)
+subplot(1,2,2)
+plot(new_values)
+
+%% Materia ficha tp4
+figure(3)
+Serie_Temporal_Sem_Tendencia=detrend(new_values,'constant');
+Tendencia=new_values-Serie_Temporal_Sem_Tendencia;
+plot((1:365),new_values,1:365,Tendencia);
+
+figure(4);
+%aproximaçõ linear de grau 2
+Coeficientes_polinomio=polyfit((1:365)',new_values,2); 
+Aproximacao_Linear_grau2=polyval(Coeficientes_polinomio,(1:365)');
+
+sinal=new_values-Aproximacao_Linear_grau2;
+plot((1:365),new_values,1:365,sinal,1:365,Aproximacao_Linear_grau2);
+
+
