decb661eeba0e6077c3c154f6200ba3f51f16483
Bruno Grifo <leitaogrifo@gmail.com>
2018-04-26 15:56:07 +0100

    deleting pl's

--- PL6/atrasa.m
+++
@@ -1,3 +0,0 @@
-function sa=atrasa(sig,samp)
-sa=[zeros(1,samp),sig(1:end-samp)];
-end
--- PL6/x4tp.m
+++
@@ -1,5 +0,0 @@
-function sig=x4tp(n)
-ix=union(find(n<-40),find(n>40));
-sig=2*sin(0.02*pi*n);
-sig(ix)=0;
-end
